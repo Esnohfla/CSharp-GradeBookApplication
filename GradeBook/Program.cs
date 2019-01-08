7bf3b47201e890e403072074a095c62a1e665f90
Esnohfla <46196134+Esnohfla@users.noreply.github.com>
2019-01-08 21:02:30 +0800

    Update Program.cs

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -8,7 +8,7 @@ namespace GradeBook
         static void Main(string[] args)
         {
             Console.WriteLine("#=======================#");
-            Console.WriteLine("# Welcome to GradeBook! #");
+            Console.WriteLine("# Welcome to GeradeBook! #");
             Console.WriteLine("#=======================#");
             Console.WriteLine();
 
