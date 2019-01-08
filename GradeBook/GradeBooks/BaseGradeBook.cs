c5b9f21a4a32332044d3cc6f76968f1d28669d54
Alfhonse <alfhonse_11@yahoo.com>
2019-01-09 00:39:12 +0800

    Update BaseGradeBook.cs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,14 +11,16 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+        public GradeBookType Type;
         public string Name { get; set; }
         public List<Student> Students { get ; set; }
-        public GradeBookType Type;
+        
 
         public BaseGradeBook(string name)
         {
             Name = name;
             Students = new List<Student>();
+
         }
 
         public void AddStudent(Student student)
